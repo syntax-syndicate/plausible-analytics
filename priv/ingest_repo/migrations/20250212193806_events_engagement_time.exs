1714f90d46704af5f3648a608bdb9af42b8cb31f
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2025-02-12 23:58:15 +0200

    Update imported_pages in migration

--- priv/ingest_repo/migrations/20250212193806_events_engagement_time.exs
+++ priv/ingest_repo/migrations/20250212193806_events_engagement_time.exs
@@ -5,5 +5,10 @@ defmodule Plausible.IngestRepo.Migrations.EventsEngagementTime do
     alter table(:events_v2) do
       add :engagement_time, :UInt32
     end
+
+    alter table(:imported_pages) do
+      add :total_time_on_page, :UInt64
+      add :total_time_on_page_visits, :UInt64
+    end
   end
 end
